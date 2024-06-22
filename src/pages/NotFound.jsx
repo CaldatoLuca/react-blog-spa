bc7ff6ecafedb14e7679745c3be0a3c91648d29e
CaldatoLuca <lucacaldato15@gmail.com>
2024-06-22 15:18:18 +0200

    ..

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center p-10">
+      <div className="flex flex-col items-center justify-center p-10 w-full h-screen">
         <div> Page Not Found</div>
 
         <div>
